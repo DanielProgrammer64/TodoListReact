1f236a40f8d6e32e076a2a4660f48aa53b8fd7e2
CodingWithSamsomDaniel <103775773+DanielProgrammer64@users.noreply.github.com>
2023-09-21 10:49:56 -0300

    src files
    ...

--- src/Lista.jsx
+++ src/Lista.jsx
@@ -1,31 +1,44 @@
 import "./Lista.css";
-import React, { useState } from "react";
-// import listpng from "./assets/listpng.png";
+import { useState } from "react";
 
 function Lista() {
+
   const [list, SetList] = useState([]); // esta é uma array vazia, que vai estocar os valores
   const [newItem, setNewItem] = useState("");
 
   function AddItem(form) {
-    form.preventDefault(); // isso impede que o formulário atualize a página
+    form.preventDefault(); // o preventDefault() impede que o formulário atualize a página se estiver vazio
     if (!newItem) {
       return; // vamos dar um return vazio, para que se não houver um novo item, ou seja, o formulário estiver vazio, nada seja retornado
     }
     SetList([...list, { text: newItem, isCompleted: false }]); // pegamos a lista vazia e adicionamos mais valores à ela.
-    setNewItem("");
-    document.getElementById("entrada").focus();
+    setNewItem(""); // configuração vazia de início
+    document.getElementById("entrada").focus(); // a entrada é o input lá em baixo
+    // o focus(), serve para destacar o elemento na página
   }
 
   function Clicked(index) {
-    const AuxList = [...list];
-    AuxList[index].isCompleted = !AuxList[index].isCompleted;
+    const AuxList = [...list]; // usamos o ... para permitir que o list seja expandido por vários elementos
+    AuxList[index].isCompleted = !AuxList[index].isCompleted; // nós usamos o isCompleted para gerenciar o state, se for true vira false e vice-versa
     SetList(AuxList);
+  }
+
+  function DeleteButton(index) {
+    const Auxlist = [...list];
+    Auxlist.splice(index); // splice serve para remover elementos de uma array, e se necessário adicionar outros, retornando os deletados
+    SetList(Auxlist); // a partir de agora, o botão deletar faz com que os elementos sejam removidos da tela quando o botão deleta é pressionado
+  }
+
+  function DeleteAll() {
+    SetList([]);
   }
 
   return (
     <div>
       <h1 style={{ fontFamily: "MontSerrat" }}> Sua TodoList </h1>
       <form onSubmit={AddItem}>
+        {" "}
+        {/*quando o formulário é submetido ao click do botão, ou ao enter, a função AddItem é executada*/}
         <input
           id="entrada"
           type="text"
@@ -61,13 +74,28 @@ function Lista() {
                 >
                   {item.text}
                 </span>
-                <button className="delete-buttons">Deletar</button>
+                <button
+                  onClick={() => {
+                    DeleteButton(index);
+                  }}
+                  className="delete-buttons"
+                >
+                  Deletar
+                </button>
               </div>
             ))
           )}
         </div>
-
-        <button className="Danger-zone"> Deletar todos ⚠️ </button>
+        {list.length > 0 && (
+          <button
+            onClick={() => {
+              DeleteAll();
+            }}
+            className="Danger-zone"
+          >
+            Deletar todos ⚠️
+          </button>
+        )}
       </div>
     </div>
   );
